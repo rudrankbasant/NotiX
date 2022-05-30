c350a42db20e9b7056a8d8be68cd07cca0868ef6
rudrankbasant <rudrank123@gmail.com>
2022-05-30 17:45:30 +0530

    feat: read notification

---
+++ app/src/main/java/com/example/notix/NotificationListener.kt
@@ -0,0 +1,40 @@
+package com.example.notix
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+import com.google.android.material.tabs.TabLayout
+
+class NotificationListener : NotificationListenerService() {
+
+    companion object{
+        const val TAG = "NotificationListener"
+    }
+
+
+    override fun onNotificationPosted(newNotification: StatusBarNotification) {
+        Log.i(
+            TAG,
+            "------------OnNotificationPosted" +
+                    "\n" + "newNotification: "+ newNotification +
+                    "\n" + "newNotification.notification: " +newNotification.notification +
+                    "\n" + "ID :" + newNotification.id +
+                    "\n" + "title: " +newNotification.notification.extras.getString("android.title") +
+                    "\n "+ "text: " +newNotification.notification.extras.getString("android.text")+
+                    "\n" + "tickerText: " + newNotification.notification.tickerText +
+                    "\n" + "postedTime: " + newNotification.postTime+
+                    "\n" + "package name: "+ newNotification.packageName
+
+
+
+
+
+            /*"-------- onNotificationPosted(): " + "ID :" + newNotification.id +
+                    "\n" + newNotification.notification.extras.getString("android.title") +
+                    "\n"+ newNotification.notification.extras.getString("android.text") +
+                    "\n"+ newNotification.notification.tickerText +
+                    "\n" + newNotification.packageName */
+
+        )
+    }
+}
